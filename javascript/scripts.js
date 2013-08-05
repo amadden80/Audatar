ea3edad912fe5f7d77ca138cadf822da203f5c25
amadden80 <amadden80@gmail.com>
2013-08-05 11:07:29 -0400

    Username Form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,25 +1,35 @@
 
+// function Audatar_audio(username){
+//     $('body').append('<script type="application/javascript" src="http://www.Sonifizer.com/api/username/'+username +'/json"></script>');
+// }
 
 
+function Audatar_audio(username){
+    // Use this if using jQuery
+    $('body').append('<script type="application/javascript" src="http://www.Sonifizer.com/api/username/'+username +'/json"></script>');
+}
 
-function get_username_audio(username){
-    
-    $.ajax({
-      url: "http://www.sonifizer.com/api/username/",
-      type: "POST",
-      data: {username: username}
-    }).done(function ( data ) {
-      if( console && console.log ) {
-        console.log("Message:", data.Sonizer);
-      }
-    });
 
+function load_username(e){
+    e.preventDefault();
+    var username = $('#username-input').val()
+    if (username.length===0){username = "Audatar"}
+    Audatar_audio(username)
+    $("#username-play-button").show()
+}
+
+function listen_to_username(e){
+    e.preventDefault();
+    Sonifizer_Play()
 }
 
 
 $(function(){
 
+    $('#username-play-button').on('click', listen_to_username)
+    $('#username-load-button').on('click', load_username)
+    $("#username-play-button").hide()
 
+});
 
-})
 
